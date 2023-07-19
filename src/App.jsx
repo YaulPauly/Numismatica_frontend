6f27673f7efd77df44908a612308f7e61913647b
Paul Yauli <pyauli6@gmail.com>
2023-07-19 01:12:50 -0500

    Added routes, created multiple pages, and set up the login structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import AuthLayout from "./layouts/AuthLayout"
+import Dashboard from "./pages/Dashboard"
+import Login from "./pages/Login"
+import SignUp from "./pages/SignUp"
+import OlvidePassword from "./pages/OlvidePassword"
+import NuevoPassword from "./pages/NuevoPassword"
+import ConfirmarCuenta from "./pages/ConfirmarCuenta"
 
 function App() {
-
   return (
-    <div className="App">
-      <h1>Uptask</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AuthLayout/>}>
+          <Route index element={<Dashboard/>}/>
+          <Route path="login" element={<Login/>}/>
+          <Route path="registrar" element={<SignUp/>}/>
+          <Route path="olvide-password" element={<OlvidePassword/>}/>
+          <Route path="olvide-password/:token" element={<NuevoPassword/>}/>
+          <Route path="confirmar/:id" element={<ConfirmarCuenta/>}/>
+
+
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/Login.jsx
@@ -0,0 +1,81 @@
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  return (
+    <>
+      <h1 className="text-6xl font-extralight text-center capitalize mb-5">
+        Numismática perú
+      </h1>
+      <h2 className="text-5xs font-extralight  text-center uppercase text-slate-800">
+        Inicia sesión y pública <br />
+        los anuncios que desees
+      </h2>
+
+      <form action="" className="my-10 bg-slate-50 rounded-lg p-10">
+        <div className="my-5">
+          <label
+            className="capitalize text-xs block font-medium"
+            htmlFor="email"
+          >
+            Correo electrónico:
+          </label>
+          <input
+            id="email"
+            type="email"
+            className="w-full mt-1 p-3 border rounded"
+          />
+        </div>
+        <div className="my-5">
+          <label
+            className="capitalize text-xs block font-medium"
+            htmlFor="password"
+          >
+            Contraseña:
+          </label>
+          <input
+            id="password"
+            type="password"
+            className="w-full mt-1 p-3 border rounded"
+          />
+        </div>
+        <div className="my-5">
+          <input
+            type="checkbox"
+            className="mr-2 leading-tight align-middle"
+            id="rememberMe"
+          />
+          <label
+            className="capitalize text-xs font-extralight cursor-pointer hover:font-light"
+            htmlFor="rememberMe"
+          >
+            Recordar Contraseña
+          </label>
+        </div>
+        <input
+          type="submit"
+          value="Iniciar Sesión"
+          className=" w-full mb-5 py-3 border capitalize font-extralight rounded 
+          hover:cursor-pointer hover:bg-slate-100 transition-colors hover:font-medium"
+        />
+      </form>
+
+      <nav className="lg:flex lg:justify-around">
+        <Link
+          className="block text-center my-5 uppercase text-sm"
+          to="/registrar"
+        >
+          ¿No tiene una cuenta? Regístrate
+        </Link>
+
+        <Link
+          className="block text-center my-5 uppercase text-sm"
+          to="/olvide-password"
+        >
+          Olvide Mi Password
+        </Link>
+      </nav>
+    </>
+  );
+};
+
+export default Login;
